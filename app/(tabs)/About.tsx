64809489b6c11a305f9689413a5c79d8014f6529
wgreunke <wgreunke@gmail.com>
2025-04-16 13:23:27 -0700

    Fixing formating

--- app/(tabs)/About.tsx
+++ app/(tabs)/About.tsx
@@ -2,15 +2,22 @@ import { View, Text, StyleSheet, Linking, Image } from "react-native";
 
 export default function About() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>About</Text>
       <Image source={require('../../assets/images/GlenTaylor.jpg')} style={styles.image} />
       <Text style={styles.description}>
-        Fleet 18 is a group of windsurfers who race on Foster City Lagoon on Tuesday Nights. After racing, we go to Waterfront Pizza for pizza and beer.
+      <Text style={styles.link} onPress={() => Linking.openURL('http://fleet18.org/')}>Fleet 18</Text>
+
+      {' '}is a group of windsurfers who race on Foster City Lagoon just south of San Francisco on Tuesday nights. After racing, we go to{' '}   
+        <Text style={styles.link} onPress={() => Linking.openURL('https://waterfrontpizza.com/')}>Waterfront Pizza</Text> 
+    
+        {' '}for pizza and beer.
       </Text>
       <Text>
         Everyone has pizza but not everyone has beer. This app helps determine how much people owe.
       </Text>
+      
+      <Text> </Text> 
       <Text>Created by: </Text>
       <Text style={styles.link} onPress={() => Linking.openURL('https://www.linkedin.com/in/wgreunke/')}>Ward Greunke</Text>
     </View>
@@ -18,6 +25,11 @@ export default function About() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   image: {
     width: 200,
     height: 200,
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -64,12 +64,7 @@ const PizzaBeerCalculator = () => {
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>Pizza and Beer Calculator for Fleet 18</Text>
       
-      <Text
-        style={styles.link}
-        onPress={() => Linking.openURL('https://waterfrontpizza.com/')}
-      >
-        Waterfront Pizza
-      </Text>
+      
 
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Pizza</Text>
@@ -77,7 +72,6 @@ const PizzaBeerCalculator = () => {
           <Text>Pizza amount without tax</Text>
           <TextInput
             keyboardType="numeric"
-            placeholder="Pizza amount without tax"
             value={pizzaPreTax}
             onChangeText={setPizzaPreTax}
             style={styles.input}
@@ -87,13 +81,12 @@ const PizzaBeerCalculator = () => {
           <Text>Number of people who ate pizza</Text>
           <TextInput
             keyboardType="numeric"
-            placeholder="Number of people who ate pizza"
             value={pizzaNum}
             onChangeText={handleIntegerInput(setPizzaNum)}
             style={styles.input}
           />
         </View>
-        <Text>Pizza per person - no tax: ${((parseFloat(pizzaPreTax) / (parseInt(pizzaNum) || 1)) || 0).toFixed(2)}</Text>
+        <Text>Pizza per person - without tax: ${((parseFloat(pizzaPreTax) / (parseInt(pizzaNum) || 1)) || 0).toFixed(2)}</Text>
       </View>
 
       <View style={styles.section}>
@@ -102,30 +95,27 @@ const PizzaBeerCalculator = () => {
           <Text>Beer amount without tax</Text>
           <TextInput
             keyboardType="numeric"
-            placeholder="Beer amount"
             value={beerPreTax}
             onChangeText={setBeerPreTax}
             style={styles.input}
           />
         </View>
         <View style={styles.inputGroup}>
-          <Text>Number of people who had pizza and beer</Text>
+          <Text>Number of people who drank beer</Text>
           <TextInput
             keyboardType="numeric"
-            placeholder="Number of people who had pizza and beer"
             value={beerNum}
             onChangeText={handleIntegerInput(setBeerNum)}
             style={styles.input}
           />
         </View>
-        <Text>Beer per person - no tax: ${((parseFloat(beerPreTax) / (parseInt(beerNum) || 1)) || 0).toFixed(2)}</Text>
+        <Text>Beer per person - without tax: ${((parseFloat(beerPreTax) / (parseInt(beerNum) || 1)) || 0).toFixed(2)}</Text>
       </View>
 
       <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Tax</Text>
+        <Text style={styles.sectionTitle}>Total Tax</Text>
         <TextInput
           keyboardType="numeric"
-          placeholder="Tax amount"
           value={tax}
           onChangeText={setTax}
           style={styles.input}
@@ -138,23 +128,23 @@ const PizzaBeerCalculator = () => {
 
       {results && (
         <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Final Numbers With Tax</Text>
-          <Text>Per Person for people having just pizza w/tax: ${results.pizzaPerPerson}</Text>
-          <Text>Per Person for beer w/tax: ${results.beerPerPerson}</Text>
-          <Text>Per Person for people having Pizza and Beer w/tax: ${results.pizzaAndBeerPerPerson}</Text>
-          <Text style={styles.validateTitle}>Validate the numbers</Text>
-          <Text>Pizza Tax: ${results.pizzaTax}</Text>
-          <Text>Beer Tax: ${results.beerTax}</Text>
-          <Text>{results.pizzaOnlyPeople} People had pizza only, paying ${results.pizzaPerPerson} for a total of ${results.pizzaOnlyTotal}.</Text>
-          <Text>{results.pizzaAndBeerPeople} People had pizza and beer, paying ${results.pizzaAndBeerPerPerson} for a total of ${results.pizzaAndBeerTotal}.</Text>
-          <Text style={styles.bold}>Total bill: ${results.totalBill}</Text>
+          <Text style={styles.sectionTitle}>Final Numbers With Tax:</Text>
+          <Text>Per Person for people who just ate pizza w/tax: ${results.pizzaPerPerson}</Text>
+          <Text>Per Person for people who just drank beer w/tax: ${results.beerPerPerson}</Text>
+          <Text>Per Person for people who ate pizza and drank beer w/tax: ${results.pizzaAndBeerPerPerson}</Text>
+          <Text style={styles.validateTitle}>Check the math:</Text>
+          {/*<Text>Pizza Tax: ${results.pizzaTax}</Text>
+          <Text>Beer Tax: ${results.beerTax}</Text>*/}
+          {/*<Text>{results.pizzaOnlyPeople} People had pizza only, paying ${results.pizzaPerPerson} for a total of ${results.pizzaOnlyTotal}.</Text> */}
+          {/*<Text>{results.pizzaAndBeerPeople} People had pizza and beer, paying ${results.pizzaAndBeerPerPerson} for a total of ${results.pizzaAndBeerTotal}.</Text>*/}
+          <Text style={styles.bold}>Your total bill should be: ${results.totalBill}</Text>
         </View>
       )}
 
       <View style={styles.footerContainer}>
-        <Text>Created by: </Text>
+        <Text>Created by: {' '}
         <Text style={styles.link} onPress={() => Linking.openURL('https://www.linkedin.com/in/wgreunke/')}>Ward Greunke</Text>
-        <Text> - </Text>
+        </Text>
 
       </View>
     </ScrollView>
